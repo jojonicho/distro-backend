5379c13db17ea1d5eca27047cfa6b4cfc20fa371
Jonathan Nicholas <jojonicho181@gmail.com>
2020-08-22 14:26:38 +0700

    fix(sendrefreshtoken): secure:true, samesite:none

--- src/utils/sendRefreshToken.ts
+++ src/utils/sendRefreshToken.ts
@@ -11,8 +11,9 @@ import { Response } from "express";
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("jid", token, {
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
+    sameSite: "none",
+    secure: true,
     path: "/refresh_token",
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
   });
 };
